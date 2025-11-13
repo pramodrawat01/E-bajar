57107ec66f4885ddab43ea326cba806a4aa58c7f
PramodRawat <rawatpramod392@gmail.com>
2025-11-13 15:17:35 +0530

    add get order changes

--- backend/Routers/Order.js
+++ backend/Routers/Order.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addOrder, getOrder } from "../Controllers/Order.js";
+import authMiddleware from "../middlewares/authmiddleware.js";
 
  const orderRouter= Router()
 
- orderRouter.get("/getOrder", getOrder)
- orderRouter.post("/addOrder", addOrder)
+ orderRouter.get("/getOrder", authMiddleware, getOrder)
+ orderRouter.post("/addOrder", authMiddleware, addOrder)
 
  export default orderRouter 
